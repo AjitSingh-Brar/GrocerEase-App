4afd2c7b862d7d0b98d08c48b3e23f15899f9516
AjitSingh-Brar <ajitsinghbrar0025@gmail.com>
2025-07-24 19:42:12 -0400

    ADD: Navbar component implemented

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import {
+  IonIcon,
+  IonHeader,
+  IonNav,
+  IonContent,
+  IonSearchbar,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonImg,
+  IonToolbar,
+  IonButton,
+  IonBadge,
+} from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
+import { cartOutline, star, personCircleOutline } from 'ionicons/icons';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.scss'],
+  standalone: true,
+  imports: [
+    IonBadge,
+    IonButton,
+    IonToolbar,
+    IonImg,
+    IonCol,
+    IonRow,
+    IonGrid,
+    IonSearchbar,
+    IonNav,
+    IonHeader,
+    IonIcon,
+    IonContent,
+  ],
+})
+export class NavbarComponent implements OnInit {
+  constructor() {
+    addIcons({personCircleOutline,cartOutline,star});
+  }
+
+  ngOnInit() {}
+}
